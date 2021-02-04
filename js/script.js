0ab8e4a09e321733387813c023ff3dcf2a79801f
Your NameWeb-devalla <kolesnikalla91@ukr.net>
2021-02-04 21:15:13 +0200

    second variant

--- js/script.js
+++ js/script.js
@@ -25,6 +25,29 @@ const movieDB = {
 };
 
 
-const promoAdv = document.querySelector('.promo__adv');
+const promoAdv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
 
-promoAdv.remove();
+
+promoAdv.forEach(item =>{
+    item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = "url('img/bg.jpg')";
+
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i+1} ${film}
+    <div class="delete"></div>
+    </li>
+    `;
+});
